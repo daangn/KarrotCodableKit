48cd530636035e78fd38ba16e5fe67c935bac3ad
Elon Park <elon.park@daangn.com>
2025-07-23 19:47:45 +0900

    refactor: #expect(is) + if let 패턴을 #require로 교체
    Swift Testing의 권장 사항에 따라 타입 체크와 캐스팅을 분리하던 패턴을
    #require를 사용하여 한 번에 처리하도록 리팩토링
    
    - OptionalPolymorphicValueResilientTests 수정
    - LossyOptionalPolymorphicValueResilientTests 수정

--- Tests/KarrotCodableKitTests/PolymorphicCodable/OptionalValue/LossyOptionalPolymorphicValueResilientTests.swift
+++ Tests/KarrotCodableKitTests/PolymorphicCodable/OptionalValue/LossyOptionalPolymorphicValueResilientTests.swift
@@ -7,98 +7,96 @@ struct LossyOptionalPolymorphicValueResilientTests {
   struct Fixture: Decodable {
     @DummyNotice.LossyOptionalPolymorphic var notice: DummyNotice?
   }
-  
+
   @Test("Outcome should be valueWasNil when decoding nil value")
-  func testNilValue() throws {
-    // Given
+  func nilValue() throws {
+    // given
     let json = """
-    {
-      "notice": null
-    }
-    """
-    
-    // When
+      {
+        "notice": null
+      }
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
     #if DEBUG
     #expect(result.$notice.outcome == .valueWasNil)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Outcome should be keyNotFound when key is missing")
-  func testMissingKey() throws {
-    // Given
+  func missingKey() throws {
+    // given
     let json = """
-    {}
-    """
-    
-    // When
+      {}
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
     #if DEBUG
     #expect(result.$notice.outcome == .keyNotFound)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Outcome should be decodedSuccessfully for successful decoding")
-  func testSuccessfulDecoding() throws {
-    // Given
+  func successfulDecoding() throws {
+    // given
     let json = """
-    {
-      "notice": {
-        "__typename": "callout",
-        "type": "callout",
-        "title": "Test Title",
-        "description": "Hello",
-        "icon": "icon.png"
+      {
+        "notice": {
+          "__typename": "callout",
+          "type": "callout",
+          "title": "Test Title",
+          "description": "Hello",
+          "icon": "icon.png"
+        }
       }
-    }
-    """
-    
-    // When
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
-    #expect(result.notice is DummyCallout)
-    if let callout = result.notice as? DummyCallout {
-      #expect(callout.type == .callout)
-      #expect(callout.title == "Test Title")
-      #expect(callout.description == "Hello")
-    }
-    
+
+    // then
+    let callout = try #require(result.notice as? DummyCallout)
+    #expect(callout.type == .callout)
+    #expect(callout.title == "Test Title")
+    #expect(callout.description == "Hello")
+
     #if DEBUG
     #expect(result.$notice.outcome == .decodedSuccessfully)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Should return nil and record error for unknown type")
-  func testUnknownTypeReturnsNil() throws {
-    // Given
+  func unknownTypeReturnsNil() throws {
+    // given
     let json = """
-    {
-      "notice": {
-        "type": "unknown-type"
+      {
+        "notice": {
+          "type": "unknown-type"
+        }
       }
-    }
-    """
-    
-    // When
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
-    
+
     #if DEBUG
     if case .recoveredFrom(let error, _) = result.$notice.outcome {
       #expect(error is DecodingError)
@@ -108,23 +106,23 @@ struct LossyOptionalPolymorphicValueResilientTests {
     #expect(result.$notice.error != nil)
     #endif
   }
-  
+
   @Test("Should return nil and record error for invalid JSON format")
-  func testInvalidJSONReturnsNil() throws {
-    // Given
+  func invalidJSONReturnsNil() throws {
+    // given
     let json = """
-    {
-      "notice": "invalid"
-    }
-    """
-    
-    // When
+      {
+        "notice": "invalid"
+      }
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
-    
+
     #if DEBUG
     if case .recoveredFrom(let error, _) = result.$notice.outcome {
       #expect(error is DecodingError)
@@ -134,28 +132,28 @@ struct LossyOptionalPolymorphicValueResilientTests {
     #expect(result.$notice.error != nil)
     #endif
   }
-  
+
   @Test("Error reporter should be called")
-  func testErrorReporterCalled() throws {
-    // Given
+  func errorReporterCalled() throws {
+    // given
     let json = """
-    {
-      "notice": {
-        "type": "unknown-type"
+      {
+        "notice": {
+          "type": "unknown-type"
+        }
       }
-    }
-    """
-    
-    // When
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let decoder = JSONDecoder()
     let errorReporter = decoder.enableResilientDecodingErrorReporting()
-    
+
     let result = try decoder.decode(Fixture.self, from: data)
- 
-    // Then
+
+    // then
     #expect(result.notice == nil)
-    
+
     let errorDigest = errorReporter.flushReportedErrors()
     #expect(errorDigest != nil)
     if let digest = errorDigest {
--- Tests/KarrotCodableKitTests/PolymorphicCodable/OptionalValue/OptionalPolymorphicValueResilientTests.swift
+++ Tests/KarrotCodableKitTests/PolymorphicCodable/OptionalValue/OptionalPolymorphicValueResilientTests.swift
@@ -7,113 +7,111 @@ struct OptionalPolymorphicValueResilientTests {
   struct Fixture: Decodable {
     @DummyNotice.OptionalPolymorphic var notice: DummyNotice?
   }
-  
+
   @Test("Outcome should be valueWasNil when decoding nil value")
-  func testNilValue() throws {
-    // Given
+  func nilValue() throws {
+    // given
     let json = """
-    {
-      "notice": null
-    }
-    """
-    
-    // When
+      {
+        "notice": null
+      }
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
     #if DEBUG
     #expect(result.$notice.outcome == .valueWasNil)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Outcome should be keyNotFound when key is missing")
-  func testMissingKey() throws {
-    // Given
+  func missingKey() throws {
+    // given
     let json = """
-    {}
-    """
-    
-    // When
+      {}
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
+
+    // then
     #expect(result.notice == nil)
     #if DEBUG
     #expect(result.$notice.outcome == .keyNotFound)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Outcome should be decodedSuccessfully for successful decoding")
-  func testSuccessfulDecoding() throws {
-    // Given
+  func successfulDecoding() throws {
+    // given
     let json = """
-    {
-      "notice": {
-        "__typename": "callout",
-        "type": "callout",
-        "title": "Test Title",
-        "description": "Hello",
-        "icon": "icon.png"
+      {
+        "notice": {
+          "__typename": "callout",
+          "type": "callout",
+          "title": "Test Title",
+          "description": "Hello",
+          "icon": "icon.png"
+        }
       }
-    }
-    """
-    
-    // When
+      """
+
+    // when
     let data = json.data(using: .utf8)!
     let result = try JSONDecoder().decode(Fixture.self, from: data)
-    
-    // Then
-    #expect(result.notice is DummyCallout)
-    if let callout = result.notice as? DummyCallout {
-      #expect(callout.type == .callout)
-      #expect(callout.title == "Test Title")
-      #expect(callout.description == "Hello")
-    }
-    
+
+    // then
+    let callout = try #require(result.notice as? DummyCallout)
+    #expect(callout.type == .callout)
+    #expect(callout.title == "Test Title")
+    #expect(callout.description == "Hello")
+
     #if DEBUG
     #expect(result.$notice.outcome == .decodedSuccessfully)
     #expect(result.$notice.error == nil)
     #endif
   }
-  
+
   @Test("Should throw error for unknown type")
-  func testUnknownTypeThrowsError() throws {
-    // Given
+  func unknownTypeThrowsError() throws {
+    // given
     let json = """
-    {
-      "notice": {
-        "__typename": "unknown",
-        "id": "1"
+      {
+        "notice": {
+          "__typename": "unknown",
+          "id": "1"
+        }
       }
-    }
-    """
-    
-    // When/Then
+      """
+
+    // when/Then
     let data = json.data(using: .utf8)!
     #expect(throws: Error.self) {
       _ = try JSONDecoder().decode(Fixture.self, from: data)
     }
   }
-  
+
   @Test("Should throw error for invalid JSON format")
-  func testInvalidJSONThrowsError() throws {
-    // Given
+  func invalidJSONThrowsError() throws {
+    // given
     let json = """
-    {
-      "notice": "invalid"
-    }
-    """
-    
-    // When/Then
+      {
+        "notice": "invalid"
+      }
+      """
+
+    // when/Then
     let data = json.data(using: .utf8)!
     #expect(throws: Error.self) {
       _ = try JSONDecoder().decode(Fixture.self, from: data)
     }
   }
-  
+
 }
